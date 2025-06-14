535396d762992b0c7d0a05187a83f34b76251ad0
Paritosh Verma <90828242+theparitoshverma@users.noreply.github.com>
2025-06-15 04:26:21 +0530

    Update enhanced-sections.tsx

--- paritosh-portfolio/components/enhanced-sections.tsx
+++ paritosh-portfolio/components/enhanced-sections.tsx
@@ -233,7 +233,16 @@ export function EnhancedSections({ skills, projects, animatedElements }: Enhance
               >
                 <CardHeader>
                   <CardTitle className="text-lg font-semibold text-gray-800 dark:text-gray-200">
-                    Hotstar Clone Bootcamp
+                    <a href="/Profile.jpg" target="_blank" rel="noopener noreferrer">
+  <span className="cursor-pointer text-blue-600 hover:underline">Hotstar Clone Bootcamp</span>
+</a>
+
+<img
+  src="/.jpg?height=192&width=192"
+  alt="Paritosh Verma"
+  className="w-full h-full object-cover rounded-full hover:scale-110 transition-transform duration-500"
+/>
+
                   </CardTitle>
                   <CardDescription className="text-blue-600 dark:text-blue-400">
                     Certified by Google, Microsoft, DevTown & Amazon
